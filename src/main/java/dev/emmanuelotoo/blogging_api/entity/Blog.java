4e78407a23da6104b816c914b9889bc4a4b72772
Emmanuel <eotoo218@gmail.com>
2025-10-21 11:32:00 +0000

    added Lombok dependency and replaced boilerplate code in Blog entity with Lombok annotations

--- src/main/java/dev/emmanuelotoo/blogging_api/entity/Blog.java
+++ src/main/java/dev/emmanuelotoo/blogging_api/entity/Blog.java
@@ -1,13 +1,20 @@
 package dev.emmanuelotoo.blogging_api.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Blog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,68 +26,4 @@ public class Blog {
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "JSON")
     private List<String> tags;
-
-    public Blog() { // my default constructor required by JPA
-    }
-
-    // Constructors, getters and setters
-    public Blog(String title, String content, String category, List<String> tags) {
-        this.title = title;
-        this.content = content;
-        this.category = category;
-        this.tags = tags;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    // Overriding the equals() and hashCode() methods
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Blog blog = (Blog) o;
-        return Objects.equals(title, blog.title) && Objects.equals(content, blog.content) && Objects.equals(category, blog.category) && Objects.equals(tags, blog.tags);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, content, category, tags);
-    }
-} // Following the POJO pattern to represent data structures
+}
